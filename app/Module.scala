3437c9047c973cda483d6d90853102555612c50f
anpaulin <ariel.paulin@gmail.com>
2026-08-20 22:49:02 -0400

    Bind AuditLogRepository and OrderService as eager singletons on startup

--- app/Module.scala
+++ app/Module.scala
@@ -11,15 +11,16 @@ class Module(environment: Environment, configuration: Configuration) extends Abs
     val auditLogType = configuration.getOptional[String]("app.audit-log.type").getOrElse("file")
 
     auditLogType.toLowerCase match {
-      case "memory" => bind(classOf[AuditLogRepository]).to(classOf[InMemoryAuditLogRepository])
-      case "kafka"  => bind(classOf[AuditLogRepository]).to(classOf[KafkaAuditLogRepository])
-      case "mysql"  => bind(classOf[AuditLogRepository]).to(classOf[MySqlAuditLogRepository])
-      case "file"   => bind(classOf[AuditLogRepository]).to(classOf[FileAuditLogRepository])
+      case "memory" => bind(classOf[AuditLogRepository]).to(classOf[InMemoryAuditLogRepository]).asEagerSingleton()
+      case "kafka"  => bind(classOf[AuditLogRepository]).to(classOf[KafkaAuditLogRepository]).asEagerSingleton()
+      case "mysql"  => bind(classOf[AuditLogRepository]).to(classOf[MySqlAuditLogRepository]).asEagerSingleton()
+      case "file"   => bind(classOf[AuditLogRepository]).to(classOf[FileAuditLogRepository]).asEagerSingleton()
       case other    => throw new IllegalArgumentException(s"Unknown audit log type '$other'. Valid options are: file, memory, kafka, mysql")
     }
 
-    bind(classOf[OrderService]).to(classOf[InMemoryOrderService])
+    bind(classOf[OrderService]).to(classOf[InMemoryOrderService]).asEagerSingleton()
   }
 }
+
 
 
